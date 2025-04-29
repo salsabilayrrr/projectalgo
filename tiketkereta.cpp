88ce3a0a297adeb1285697557fdf11eea116ed16
salsabilayrrr <salsabilayufliramadhani15@gmail.com>
2025-04-29 10:19:51 +0700

    commmit 1

---
+++ tiketkereta.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct TiketKereta {
+    string id_tiket;
+    string nama_penumpang;
+    string asal;
+    string tujuan;
+    string tanggal;
+    string jam;
+    string kelas;
+    int kursi;
+    TiketKereta* next;
+};
+
+TiketKereta* head = nullptr;
